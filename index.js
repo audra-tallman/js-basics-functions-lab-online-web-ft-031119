62da2e7daeb8c8ec6e876ae221da6da5e6c34249
Audra Tallman <audi.tallman@gmail.com>
2019-06-19 16:14:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start, end) {
+  let block = 264 feet
+  
+}
+
+
+function distanceFromHqInFeet() {
+  
+}
+
+
+function distanceTravelledInFeet() {
+  
+}
+
+
+function calculatesFarePrice(start, destination) {
+  
+}
